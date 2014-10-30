95f9cb96b54fafef3edf3b6798ce24736eb452cd
OscarGarciaRuiz <oscar.luis.garcia94@gmail.com>
2014-10-30 13:09:59 -0600

    Diseño para los admins

--- app/views/restaurant/create.blade.php
+++ app/views/restaurant/create.blade.php
@@ -1,42 +1,43 @@
 
-@extends('layouts.master')
+@extends('layouts.slash')
 
 @section('contenido')
-<div style="text-align: left">{{link_to('restaurants','Atras')}}</div>
-
-<center>
-<h1> Alta de Restaurante</h1>
-
-    {{Form::open(['route'=> 'restaurant.store'])}}
-
-
-       <div> {{Form::label('name','Escribe el Nombre del restaurante:')}}
-        {{Form::input('text','name')}}
-        {{$errors->first('name')}}
-</div><div>
-        {{Form::label('textaddress','Direcciion')}}
-        {{Form::input('text','textaddress')}}
-                {{$errors->first('textaddress')}}
-
-</div>
-  <div> {{Form::label('onlycash','Si solo aceptara efectivo (0 o 1)')}}
-        {{Form::input('text','onlycash')}}
-        {{$errors->first('onlycash')}}
-</div>
-<div> {{Form::label('type','Tipo del Restaurant:')}}
-        {{Form::input('text','type')}}
-        {{$errors->first('type')}}
-</div>
-<div> {{Form::label('description','Escribe alguan descripcion:')}}
-        {{Form::input('text','description')}}
-        {{$errors->first('type')}}
-</div>
-<div>
-
-{{Form::submit('Grabar')}}
-</div>
-
-    {{Form::close()}}
-</center>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-md-offset-4">
+                <div class="login-panel panel panel-default">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Alta de restaurante</h3>
+                    </div>
+                        <div class="panel-body">
+                            {{Form::open(['route'=> 'restaurant.store'])}}
+                                <div class="form-group">
+                                    {{Form::input('text','name', null,array('class'=>'form-control','placeholder'=>'Nombre del Restaurante'))}}
+                                    {{$errors->first('name')}}
+                                </div>
+                                <div class="form-group">
+                                    {{Form::input('text','textaddress', null,array('class'=>'form-control','placeholder'=>'Ingrese la Direccion'))}}
+                                    {{$errors->first('textaddress')}}
+                                </div>
+                                <div class="form-group">
+                                    {{Form::input('text','onlycash', null,array('class'=>'form-control','placeholder'=>'Solo Efectivo (1 o 0)'))}}
+                                    {{$errors->first('onlycash')}}
+                                </div>
+                                <div class="form-group">
+                                    {{Form::input('text','type', null,array('class'=>'form-control','placeholder'=>'Ingrese Tipo del Restaurante'))}}
+                                    {{$errors->first('type')}}
+                                </div>
+                                <div class="form-group">
+                                    {{Form::input('text','description', null,array('class'=>'form-control','placeholder'=>'Descripcion'))}}
+                                    {{$errors->first('type')}}
+                                </div>
+                                <div class="form-group">
+                                    {{Form::submit('Grabar',array('class'=>'btn btn-lg btn-success btn-block'))}}
+                                </div>
+                                    {{Form::close()}}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
   @stop
-
