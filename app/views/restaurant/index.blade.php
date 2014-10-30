370d5b933ca64e6e38f7ba591abb72c12c99fed7
OscarGarciaRuiz <oscar.luis.garcia94@gmail.com>
2014-10-30 14:38:13 -0600

    Diseño

--- app/views/restaurant/index.blade.php
+++ app/views/restaurant/index.blade.php
@@ -8,15 +8,7 @@
 @section('ocho')
 
                 <div class="col-lg-8">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            <i class="fa fa-clock-o fa-fw"></i> Timeline
-                        </div>
-                        <!-- /.panel-heading -->
-                        <div class="panel-body">
-                        </div>
-                        <!-- /.panel-body -->
-                    </div>
+
                     <!-- /.panel -->
                 </div>
 
