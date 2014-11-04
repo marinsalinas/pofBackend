3300254b0896e1473ca7439ceec9ec1bbe2c11b3
Marin Salinas <m.mslns27@gmail.com>
2014-11-04 11:33:28 -0600

    Favicon con Script de analitycs

--- app/views/index/index.blade.php
+++ app/views/index/index.blade.php
@@ -7,8 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="shortcut icon" href="index/img/favicon.ico" type="image/x-icon">
-    <link rel="icon" href="index/img/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="icon" href=" favicon.ico" type="image/x-icon">
     <title>Path Of The Food</title>
 
 
@@ -225,5 +225,15 @@ $(function(){
 
 	});
 	</script>
+	<script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-56372831-1', 'auto');
+      ga('send', 'pageview');
+
+    </script>
 </body>
 </html>
