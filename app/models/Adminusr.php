8aed1f5b68f7cb4f81cc311bf972413411160e7a
OscarGarciaRuiz <oscar.luis.garcia94@gmail.com>
2014-11-17 10:15:54 -0600

    image_url

---
+++ app/models/Adminusr.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Auth\UserTrait;
+use Illuminate\Auth\UserInterface;
+use Illuminate\Auth\Reminders\RemindableTrait;
+use Illuminate\Auth\Reminders\RemindableInterface;
+
+class User extends Eloquent implements UserInterface, RemindableInterface {
+    use UserTrait, RemindableTrait;
+
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'adminusers';
+
+    protected $fillable = ['username','password','fullname','email','phone'];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+
+    protected $hidden = array('password', 'remember_token');
+
+    public function address(){
+        return $this->hasMany('Address', 'user_id');
+    }
+
+
+}
