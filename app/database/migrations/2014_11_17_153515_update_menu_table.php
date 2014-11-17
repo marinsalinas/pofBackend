4147dcd0b6f753180201904bbbe577d97e94608b
OscarGarciaRuiz <oscar.luis.garcia94@gmail.com>
2014-11-17 09:41:46 -0600

    image_url

---
+++ app/database/migrations/2014_11_17_153515_update_menu_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateMenuTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('menus', function(Blueprint $table)
+		{
+            $table->string('image_url');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('menus', function(Blueprint $table)
+		{
+            $table->dropColumn('image_url');
+        });
+	}
+
+}
