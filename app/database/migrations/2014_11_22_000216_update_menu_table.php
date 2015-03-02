4958d9b744e3c342e8890b30b67d60ab3fb1ea11
Marín Salinas <marinssalinas@gmail.com>
2015-03-01 21:48:47 -0600

    Configuración de la Tabla Menu

--- app/database/migrations/2014_11_22_000216_update_menu_table.php
+++ app/database/migrations/2014_11_22_000216_update_menu_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class UpdateMenuTable extends Migration {
+class UpdateMenuTable2s extends Migration {
 
 	/**
 	 * Run the migrations.
