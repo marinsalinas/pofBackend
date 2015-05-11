3751dee7d7df2bc3af744744c089c1859f27e795
OscarGarciaRuiz <oscar.luis.garcia94@gmail.com>
2015-05-11 11:56:55 -0500

    Esperando opiniones log in

--- app/views/layouts/login.blade.php
+++ app/views/layouts/login.blade.php
@@ -1,32 +1,55 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
+  <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="author" content="Dashboard">
+    <meta name="keyword" content="Dashboard, Bootstrap, Admin, Template, Theme, Responsive, Fluid, Retina">
 
-
-
-    <title>Log in</title>
+    <title>DASHGUM - Bootstrap Admin Template</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    <!--external css-->
+    <link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
 
     <!-- Custom styles for this template -->
-    <link href="css/signin.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
+    <link href="assets/css/style-responsive.css" rel="stylesheet">
 
-</head>
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
 
-<body>
+  <body>
 
-<div class="container">
+      <!-- **********************************************************************************************************************************************************
+      MAIN CONTENT
+      *********************************************************************************************************************************************************** -->
 
-   @yield('login')
+	  <div id="login-page">
+	  	<div class="container">
 
-</div> <!-- /container -->
+        @yield('login')
 
-</body>
+	  	</div>
+	  </div>
+
+    <!-- js placed at the end of the document so the pages load faster -->
+    <script src="assets/js/jquery.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+
+    <!--BACKSTRETCH-->
+    <!-- You can use an image of whatever size. This script will stretch to fit in any screen size.-->
+    <script type="text/javascript" src="assets/js/jquery.backstretch.min.js"></script>
+    <script>
+        $.backstretch("assets/img/rest.jpg", {speed: 500});
+    </script>
+
+
+  </body>
 </html>
--- app/views/sessions/login.blade.php
+++ app/views/sessions/login.blade.php
@@ -2,16 +2,20 @@
 @section('login')
 
 
-{{Form::open(['url' => 'store','class'=>'form-signin'])}}
-        <h2 class="form-signin-heading" style="color: #ffffff">Acceso al sitio</h2>
-        <label for="inputEmail" class="sr-only">Email address</label>
+{{Form::open(['url' => 'store','class'=>'form-login'])}}
+		        <h2 class="form-login-heading">
+		        <img src="img/poffull2.png" style="max-width:20%;
+                                                            max-height:100%; position: relative; margin-bottom: 10px;"></br>
+Accede al sitio
+		        </h2>
 
+		        <div class="login-wrap">
 {{Form::text('username', null,array('class'=>'form-control','placeholder'=>'Username','id'=>'inputEmail'))}}
-        <label for="inputPassword" class="sr-only">Password</label>
-
+		            <br>
 {{Form::password('password',array('class'=>'form-control','placeholder'=>'Password','id'=>"inputPassword"))}}
-
-{{Form::submit('Acceder',array('class'=>'btn btn-lg btn-primary btn-block'))}}
+<br>
+{{Form::submit('Acceder',array('class'=>'btn btn-theme btn-block'))}}
+</div>
 
 {{Form::close()}}
 @stop
